83a730cb4cec7ec228395532b8c0d3eb3e81fafd
John Kenny <johnkenny54@yahoo.com>
2023-04-28 11:15:05 -0700

    Add overrides for native CA plants introduced to East Bay.

--- js/tools.js
+++
@@ -1,8 +0,0 @@
-import { DataLoader, Taxa } from "@ca-plant-list/ca-plant-list";
-import { Tools } from "@ca-plant-list/tools/Tools";
-
-const options = Tools.getCommandLineOptions( DataLoader.getOptionDefs() );
-
-await DataLoader.load( options );
-
-Tools.run( options, Taxa );
